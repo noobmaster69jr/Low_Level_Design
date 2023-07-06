f8729c87eb16d9ad90d084557aaad157e7226631
noobmaster69jr <srashwin22@gmail.com>
2023-07-06 23:04:18 +0530

    added overloading

---
+++ src/overloading/A.java
@@ -0,0 +1,11 @@
+package overloading;
+
+public class A {
+	void print() {
+		System.out.println("Before overloading");
+	}
+
+	void print(String name) {
+		System.out.println("After overloading ");
+	}
+}
